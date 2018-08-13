3baa62c5315d998073c5e27b781750806e26ead0
endsound <endsound0211@gmail.com>
2018-08-13 16:19:04 +0800

    export token

--- public_api.ts
+++ public_api.ts
@@ -1,1 +1,4 @@
 export * from './src/app/auto-validate/auto-validate.module'
+export * from './src/app/auto-validate/error-message-map-token'
+export * from './src/app/auto-validate/locale/default-error-message-map-en-us'
+export * from './src/app/auto-validate/locale/default-error-message-map-zh-tw'
